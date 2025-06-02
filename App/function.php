984c0a975b5406628f3b742ac3dbe718f0bce9f7
moriomnela <moriomislamnela23@gmail.com>
2025-06-02 10:44:06 +0600

    App file

--- App/function.php
+++ App/function.php
@@ -4,7 +4,7 @@
  */
   function info(string $name, int $age, string $skill){
       if( $skill ){
-       echo "Hello {$name},You are {$age} years old and you are a {$skill} devs";
+       echo "Hello {$name},You are {$age} years old and you are a {$skill}";
     }else {
          echo "Hello {$name}, You are {$age} years old";
       }
--- App/index.php
+++ App/index.php
@@ -3,7 +3,8 @@
     require_once __DIR__. '/autoload.php';
    }
 
-   info('Moriom', 20 ,"PHP" );
+   info('Moriom', 20 ,"PHP. <br>");
+   info('Rifat', 10 ,"Laravel." );
 
 
 ?>
